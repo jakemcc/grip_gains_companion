97e0003129d9fb295d62a115ce9b597dc71912bf
Jake McCrary <jake@jakemccrary.com>
2026-08-05 08:45:16 -0500

    bump version

--- android/grip_gains_companion/app/build.gradle.kts
+++ android/grip_gains_companion/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.grip_gains_auto"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
